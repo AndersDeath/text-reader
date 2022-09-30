5e2c0db53900228e9af91248e32a3b210a771350
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-09-30 17:00:21 +0300

    update

--- src/app/pages/links-page/links-page.component.ts
+++ src/app/pages/links-page/links-page.component.ts
@@ -1,16 +1,38 @@
 import { AuthService } from './../../shared/services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
+@UntilDestroy()
 @Component({
   selector: 'app-links-page',
   templateUrl: './links-page.component.html',
   styleUrls: ['./links-page.component.scss']
 })
 export class LinksPageComponent implements OnInit {
+  public userData: any;
+  messages: any[] = [];
 
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userData = this.authService.userData;
+    try {
+      this.authService.authState().subscribe((state) => {
+        this.userData = state?.toJSON()
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    try {
+      this.authService.getTexts(this.userData)
+      .pipe(untilDestroyed(this)).subscribe((w) => {
+        w.forEach((e) => {
+          this.messages.push(e.data());
+        });
+      });
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   logout() {
