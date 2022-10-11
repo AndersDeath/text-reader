a8c553438c6be4ee41406ebe674c213ca1619e21
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-10-11 16:53:55 +0200

    update

--- src/app/pages/links-page/links-page.component.ts
+++ src/app/pages/links-page/links-page.component.ts
@@ -1,6 +1,10 @@
 import { AuthService } from './../../shared/services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import {MatSort, Sort} from '@angular/material/sort';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+
 
 @UntilDestroy()
 @Component({
@@ -12,7 +16,16 @@ export class LinksPageComponent implements OnInit {
   public userData: any;
   messages: any[] = [];
 
-  constructor(private authService: AuthService) { }
+  displayedColumns: string[] = ['icon', 'title', 'date', 'url'];
+  public dataSource: any = []
+  @ViewChild(MatSort) sort: MatSort;
+
+  @ViewChild(MatTable) table: MatTable<any>;
+
+  constructor(
+    private authService: AuthService,
+    private _liveAnnouncer: LiveAnnouncer
+  ) { }
 
   ngOnInit(): void {
     this.userData = this.authService.userData;
@@ -36,7 +49,14 @@ export class LinksPageComponent implements OnInit {
             icon: url.origin,
             date: new Date(data.date)
           });
+          this.dataSource.push({
+            url: data.message,
+            title: data.title,
+            icon: url.origin,
+            date: new Date(data.date)
+          })
         });
+        // this.dataSource =  new MatTableDataSource(this.messages);
       });
     } catch(e) {
       console.log(e);
@@ -45,6 +65,24 @@ export class LinksPageComponent implements OnInit {
 
   logout() {
     this.authService.signOut();
+  }
+
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
