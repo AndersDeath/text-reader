467732147f0e073a15eef0ebd2fbad0d6ddd6e31
Vladimir Bolshakov <andersdeath@yandex.ru>
2022-11-28 11:38:07 +0300

    update

--- src/app/pages/press-page/press-page.component.ts
+++ src/app/pages/press-page/press-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PressPageComponent implements OnInit {
 
+  public links: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
